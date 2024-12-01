8b4ba61546e2f3133174261995296abd6da3fed3
Grady <arrangedgodly@gmail.com>
2024-12-01 09:09:54 -0700

    slightly shrank the tileset size and now we are having less issues with spacing

--- gurpygames.gd
+++ gurpygames.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-@onready var camera_2d: Camera2D = $Camera2D
+@onready var camera: Camera2D = $Camera2D
 @onready var background: TileMapLayer = $Background
 @onready var detail: TileMapLayer = $Detail
 @onready var main_layer: TileMapLayer = $MainLayer
+
+func _ready() -> void:
+	camera.zoom = Vector2(2, 2)
+	var tween = create_tween()
+	tween.tween_property(camera, "zoom", Vector2(.15, .15), 3)
