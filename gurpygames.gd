c790cab6c06611e18b7b17f949c724e916283b06
Grady Wasil <90996993+Arrangedgodly@users.noreply.github.com>
2024-11-30 23:46:24 -0700

    changing the logo for more detail and curves

---
+++ gurpygames.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var background: TileMapLayer = $Background
+@onready var detail: TileMapLayer = $Detail
+@onready var main_layer: TileMapLayer = $MainLayer
